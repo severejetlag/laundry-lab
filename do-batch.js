182b57aafc4278d67dd4a675a099b6bc63b0dfc9
severejetlag <nhl1013@gmail.com>
2017-10-02 22:59:09 -0700

    do batches of laundry

--- do-batch.js
+++ do-batch.js
@@ -5,15 +5,34 @@ var batch_input = [
 ];
 
 /* TODO: copy `clean` function here */
+function clean(clothing_item){
+    // return mended_clothing_item;
+    return clothing_item.replace('*','');
+}
 /* TODO: copy `mend` function here */
+function mend(clothing_item){
+    // return mended_clothing_item;
+    return clothing_item.replace('/','');
+}
 /* TODO: copy `iron` function here */
+function iron(clothing_item){
+    // return ironed_clothing_item;
+    return clothing_item.toLowerCase();
+}
 
 
 /* YOUR CODE HERE */
 function doBatch(clothes){
     // your code here
+	clothes.forEach(function(batch){
+		batch.forEach(function(item){
+		    console.log(iron(mend(clean(item))));
+			
+		})
+	})
 }
 
+doBatch(batch_input);
 
 /* PRINT RESULT */
 console.log("--> Finished running do-batch.js");
