7d6c7e7d2e27321925180fc5be2b8c533c1288f2
severejetlag <nhl1013@gmail.com>
2017-10-02 22:19:31 -0700

    cleaned shirts.

--- clean.js
+++ clean.js
@@ -6,11 +6,15 @@ var dirty_clothes = [
   "paisl*ey dress shirt"
 ];
 
+dirty_clothes.forEach(function(item){
+	console.log(clean(item));
+})
+
 /* YOUR CODE HERE */
 function clean(clothing_item){
-    // return clean_clothing_item;
+    // return mended_clothing_item;
+    return clothing_item.replace('*','');
 }
-
 
 /* PRINT RESULT */
 console.log("--> Finished running clean.js");
