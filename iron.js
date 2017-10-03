06bba7257703776161a60d0cff77525136e4b045
severejetlag <nhl1013@gmail.com>
2017-10-02 22:03:23 -0700

    Succesfully ironed shirts.

--- iron.js
+++ iron.js
@@ -7,11 +7,18 @@ var wrinkled_clothes = [
   "whItE bLousE"
 ];
 
+wrinkled_clothes.forEach(function(clothing){
+	console.log(iron(clothing));
+})
+
+
 /* YOUR CODE HERE */
 function iron(clothing_item){
     // return ironed_clothing_item;
+    return clothing_item.toLowerCase();
 }
 
+// console.log(iron(wrinkled_clothes));
 
 /* PRINT RESULT */
 console.log("--> Finished running iron.js");
